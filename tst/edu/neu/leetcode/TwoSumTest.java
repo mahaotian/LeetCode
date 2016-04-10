c1ad6bd74ecc0f09a21649d86abc9ef8a69cdf10
Haotian <mahaotian1990@gmail.com>
2016-04-10 11:44:11 +0800

    #1 TwoSum solution

---
+++ tst/edu/neu/leetcode/TwoSumTest.java
@@ -0,0 +1,20 @@
+package edu.neu.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by haotianm on 16-4-10.
+ */
+public class TwoSumTest {
+    @Test
+    public void twoSumTest() {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = solution.twoSum(nums, target);
+        assertEquals(res[0], 0);
+        assertEquals(res[1], 1);
+    }
+}
